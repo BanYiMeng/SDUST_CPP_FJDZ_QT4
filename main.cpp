b02327d516bb940b859cf29552ce62bbee8498fb
BanYiMeng <2499642753@qq.com>
2019-06-12 00:05:40 +0800

    add cg; include fatal bug;

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
-//#include <phonon>
 #include <QWidget>
+#include <QtWebKit>
+#include <QUrl>
+
+//#include <phonon>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +12,10 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.setWindowIcon(QIcon(":/resource/logo.jpg"));
     w.setStyleSheet("border-image:url(:/resource/background.jpg);");
+    QWebView wv;
+    wv.setAttribute(Qt::WA_DeleteOnClose);
+    wv.load(QUrl("https://test-1254946716.cos.ap-chongqing.myqcloud.com/video.mp4"));
+    wv.showFullScreen();
     //Phonon::VideoPlayer *player =new Phonon::VideoPlayer(Phonon::VideoCategory,&w);
     //player->resize(800,450);
     //player->load(Phonon::MediaSource("/home/tester/文档/extra/video.mp4"));
--- untitled.pro
+++ untitled.pro
@@ -6,6 +6,8 @@
 
 QT       += core gui
 #QT       += phonon
+QT       += webkit
+QT       += network
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
