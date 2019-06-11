0adcda2fa8f64b1005a794968fa47059506724ee
BanYiMeng <2499642753@qq.com>
2019-06-11 23:06:32 +0800

    1.0a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
+//#include <phonon>
+#include <QWidget>
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +9,10 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.setWindowIcon(QIcon(":/resource/logo.jpg"));
     w.setStyleSheet("border-image:url(:/resource/background.jpg);");
+    //Phonon::VideoPlayer *player =new Phonon::VideoPlayer(Phonon::VideoCategory,&w);
+    //player->resize(800,450);
+    //player->load(Phonon::MediaSource("/home/tester/文档/extra/video.mp4"));
+    //player->play();
     w.show();
     return a.exec();
 }
