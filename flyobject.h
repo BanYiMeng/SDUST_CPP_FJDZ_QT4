18eb2aa840a29164d240b9569f98c8b86874b813
ybt1005 <wxhhwd@qq.com>
2019-06-15 17:49:55 +0800

    /y

--- flyobject.h
+++ flyobject.h
@@ -11,12 +11,13 @@ class flyobjectData;
 class flyobject
 {
 public:
-    flyobject();
+    flyobject(int xx=-1,int yy=-1,int ss=-1,int tt=-1,int ww=-1,int hh=-1):x(xx),y(yy),s(ss),t(tt),w(ww),h(hh){
+    };//x,y,s:speed,t:times,w:weight.h:hight;
     flyobject(const flyobject &);
     flyobject &operator=(const flyobject &);
     ~flyobject();
-
 private:
+    int x,y,s,t,w,h;
     QSharedDataPointer<flyobjectData> data;
 };
 
